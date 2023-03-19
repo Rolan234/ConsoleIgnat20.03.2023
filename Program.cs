b430dfc680fd3f0eb41e148cbcde15f06dc7e067
Rolan234 <124524243+Rolan234@users.noreply.github.com>
2023-03-19 22:25:19 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIgnat20._03._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Задание 1
+           
+            Console.Write("Введите количество элементов массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите элемент массива [{i}]: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int max = array[0];
+            int maxIndex = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"Максимальный элемент массива: {max}");
+            Console.WriteLine($"Индекс максимального элемента: {maxIndex}");
+        }
+    }
+}
