817898fbbaa6de0613ed406a754285d193d8d8f4
Rolan234 <124524243+Rolan234@users.noreply.github.com>
2023-03-19 22:27:36 +0200

    s

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleIgnat20._03._2023
         static void Main(string[] args)
         {
             Задание 1s
-           
+           s
             Console.Write("Введите количество элементов массива: ");
             int n = int.Parse(Console.ReadLine());
 
